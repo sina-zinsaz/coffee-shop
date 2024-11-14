d2ff9a8bf1395b6b56a1fe1c7c4bdd05ccdc16f9
Sina Zinsaz <sinazinsaz1@gmail.com>
2024-11-14 23:15:03 +0330

    clean code

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,16 +1,40 @@
 import { Box } from "@mui/material";
 import { btn } from "../interfaces/btn";
 
-
-
-export default function Btn({Title , px , py , bgcolor , color , width , height , fontSize , border  , borderRadius , mb, onClick}:btn) {
+export default function Btn({
+  Title,
+  px,
+  py,
+  bgcolor,
+  color,
+  width,
+  height,
+  fontSize,
+  border,
+  borderRadius,
+  mb,
+  onClick,
+}: btn) {
   return (
-    <Box component='button' onClick={onClick}
-        sx={{
-        bgcolor:`#${bgcolor}` , color:{color} , fontWeight:'bold'
-        , border:'none' , py:`${py}%` , px:`${px}%` , borderRadius:{borderRadius}}}
-        width={width} height={height} fontSize={fontSize} border={border} mb={mb}>
-        {Title}
+    <Box
+      component="button"
+      onClick={onClick}
+      sx={{
+        bgcolor: `#${bgcolor}`,
+        color: { color },
+        fontWeight: "bold",
+        border: "none",
+        py: `${py}%`,
+        px: `${px}%`,
+        borderRadius: { borderRadius },
+      }}
+      width={width}
+      height={height}
+      fontSize={fontSize}
+      border={border}
+      mb={mb}
+    >
+      {Title}
     </Box>
-  )
+  );
 }
