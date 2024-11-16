02b99e170e2d37039146d250d66bcc59465081c3
Sina Zinsaz <sinazinsaz1@gmail.com>
2024-11-16 14:42:50 +0330

    fix btn completed buy in home

--- src/interfaces/btn.ts
+++ src/interfaces/btn.ts
@@ -1,14 +1,7 @@
+import { SxProps, Theme } from "@mui/material";
+
 export interface btn {
     Title:string;
-    px?:number;
-    py?:number;
-    bgcolor:string;
-    color:string;
     onClick?:() => void;
-    width?:string;
-    height?:string;
-    border?:string;
-    mb?:string;
-    fontSize?:string;
-    borderRadius?:string;
+    sx?: SxProps<Theme>;
 }
