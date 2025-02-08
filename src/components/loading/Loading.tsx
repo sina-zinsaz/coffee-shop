47c735253514049aa25fecd814729a515e556bda
Sina Zinsaz <sinazinsaz1@gmail.com>
2025-02-08 22:48:10 +0330

    feat: new text for loading

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,29 +1,31 @@
 import Box from "@mui/material/Box";
-import './style.css'
+import "./style.css";
 
 export default function Loading() {
   return (
     <>
       <Box
-      component="div" 
-      display="flex"
-      width='100%'
-      height='100vh'
-      flexDirection="column"
-      justifyContent="center"
-      textAlign="center"
-    >
-      <Box component="div" sx={{ fontWeight: "700", fontSize: "larger" }}>
-        <Box component="p">open with size mobile...</Box>
+        component="div"
+        display="flex"
+        width="100%"
+        height="100vh"
+        flexDirection="column"
+        justifyContent="center"
+        textAlign="center"
+      >
+        <Box component="div" sx={{ fontWeight: "700", fontSize: "larger" }}>
+          <Box component="p">
+            This site is only available on mobile devices :)
+          </Box>
+        </Box>
+        <Box component="div" className="loading-child">
+          <Box component="div" id="square1"></Box>
+          <Box component="div" id="square2"></Box>
+          <Box component="div" id="square3"></Box>
+          <Box component="div" id="square4"></Box>
+          <Box component="div" id="square5"></Box>
+        </Box>
       </Box>
-      <Box component="div" className="loading-child">
-        <Box component="div" id="square1"></Box>
-        <Box component="div" id="square2"></Box>
-        <Box component="div" id="square3"></Box>
-        <Box component="div" id="square4"></Box>
-        <Box component="div" id="square5"></Box>
-      </Box>
-    </Box>
-  </>
-  )
+    </>
+  );
 }
